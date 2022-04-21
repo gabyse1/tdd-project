e122f78715826d56bcd6b5f6d89c9537ecec7b0a
gabyse1 <gaby.es.iaa@gmail.com>
2022-04-21 13:34:59 -0500

    Create the tests for the reverse method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -23,4 +23,15 @@ describe Solver do
       expect { @solver.factorial('5') }.to raise_exception ArgumentError
     end
   end
+
+  context '#reverse method' do
+    it 'throws an argument error if the input parameter is not a string' do
+      expect { @solver.reverse(15) }.to raise_exception ArgumentError
+    end
+
+    it 'return the input string in reverse order' do
+      reverseString = @solver.reverse('hello')
+      expect(reverseString).to eql 'olleh'
+    end
+  end 
 end
