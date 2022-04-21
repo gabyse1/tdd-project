a8c7e69d0f5e43acde2ca439c70ce0a7f0970a21
gabyse1 <gaby.es.iaa@gmail.com>
2022-04-21 13:10:48 -0500

    Create an instance of Solver class in the test file to call the class methods

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,23 +1,26 @@
 require_relative '../solver'
 
-Describe Solver do
+describe Solver do
+  before :all do
+    @solver = Solver.new
+  end
   context '#Factorial method' do
     it 'returns the factorial of a positive integer' do
-      factorial = factorial(5)
+      factorial = @solver.factorial(5)
       expect(factorial).to eql 120
     end
 
     it 'returns the factorial of zero' do
-      factorial = factorial(0)
+      factorial = @solver.factorial(0)
       expect(factorial).to eql 0
     end
 
     it 'throws an argument error if the input parameter is a negative integer' do
-      expect { factorial(-5) }.to raise_exception ArgumentError
+      expect { @solver.factorial(-5) }.to raise_exception ArgumentError
     end
 
     it 'throws an argument error if the input parameter is a string' do
-      expect { factorial('5') }.to raise_exception ArgumentError
+      expect { @solver.factorial('5') }.to raise_exception ArgumentError
     end
   end
 end
